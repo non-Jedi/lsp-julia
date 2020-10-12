cc21f7de428164df45321d4910d650fe6dc20f5a
Or Dicker <or.dicker@gmail.com>
2020-10-12 14:53:16 +0300

    add simple tests

--- tests/test-lsp-julia.el
+++ tests/test-lsp-julia.el
@@ -1,6 +1,14 @@
-;;; test.el --- tests for elfeed -*- lexical-binding: t; -*-
+;;; test-lsp-julia.el --- tests for lsp-julia -*- lexical-binding: t; -*-
 
 (require 'ert)
+(require 'lsp-julia)
 
-(ert-deftest test-simple ()
-  (should (equal 4 (/ 8 2))))
+(ert-deftest lsp-julia-files-and-cmd-test ()
+  (should (and (f-exists? lsp-julia-package-dir)
+	       (not (eq (executable-find "julia") nil))
+	       )))
+
+(ert-deftest lsp-julia-server-test ()
+  (find-file "./tests/sample.jl")
+  (should (equal (lsp-buffer-language) "julia")))
+;;; test-lsp-julia.el ends here
